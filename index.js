7662f03f42a8152ac1e9931a23c37040c8c053e0
codernoyan <coder.noyan@gmail.com>
2022-11-25 03:28:56 +0600

    products post api created

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// verify jwt middleware
+const verifyJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ access: 'Unauthorized' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ access: 'Forbidden' });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 // mongodb
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ufdxsbo.mongodb.net/?retryWrites=true&w=majority`;
@@ -80,7 +96,26 @@ app.get('/users', async (req, res) => {
   try {
     const query = {};
     const users = await usersCollection.find(query).toArray();
+
     res.send(users);
+
+  } catch (error) {
+    res.send({
+      success: false,
+      error: error.message
+    })
+  }
+});
+
+// product post
+app.post('/products', async (req, res) => {
+  try {
+    const productsData = req.body;
+    console.log(productsData);
+    const result = await productCollection.insertOne(productsData);
+    
+    res.send(result);
+
   } catch (error) {
     res.send({
       success: false,
